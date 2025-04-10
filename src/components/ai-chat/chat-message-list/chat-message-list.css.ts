1c7fafd3257d5b7e01305794832f10f870f57aa4
RenuPE101 <renu.ganvir@pharmeasy.in>
2025-04-10 19:29:06 +0530

    folder structure updated

---
+++ src/components/ai-chat/chat-message-list/chat-message-list.css.ts
@@ -0,0 +1,89 @@
+import { css } from "lit";
+
+export const styles = css`
+	.chat-container {
+		background-color: #ffffff;
+		padding: 0 16px;
+		overflow-y: auto;
+		height: calc(100vh - 142px);
+	}
+
+	.bot-message-container {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+		margin-bottom: 24px;
+	}
+
+	.user-message-container {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+		margin-bottom: 24px;
+	}
+
+	.bot-message-content {
+		display: flex;
+		gap: 8px;
+	}
+
+	.bot-message {
+		width: 251px;
+		border-top-left-radius: 2px;
+		border-top-right-radius: 16px;
+		border-bottom-right-radius: 16px;
+		border-bottom-left-radius: 16px;
+		border-width: 1px;
+		border: 1px solid #e6ebf4;
+		padding: 12px;
+		font-weight: 500;
+		font-size: 12px;
+		line-height: 18px;
+		color: #30363c;
+	}
+
+	.user-message {
+		width: 146px;
+		border-top-left-radius: 16px;
+		border-top-right-radius: 2px;
+		border-bottom-right-radius: 16px;
+		border-bottom-left-radius: 16px;
+		border-width: 1px;
+		padding: 12px;
+		border: 1px solid #b5cdf7;
+		background-color: #ebf2ff;
+		color: #30363c;
+		font-weight: 500;
+		font-size: 12px;
+		line-height: 18px;
+	}
+
+	.user-message-content {
+		display: flex;
+		gap: 8px;
+		justify-content: flex-end;
+	}
+	.user-message-container {
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+		margin-bottom: 24px;
+	}
+
+	.bot-timestamp {
+		font-weight: 400;
+		font-size: 10px;
+		line-height: 16px;
+		text-align: left;
+		margin-left: 48px;
+		color: #6e787e;
+	}
+
+	.user-timestamp {
+		font-weight: 400;
+		font-size: 10px;
+		line-height: 16px;
+		text-align: right;
+		color: #6e787e;
+	}
+`;
---
+++ src/components/ai-chat/timestamp-divider/timestamp-divider.css.ts
@@ -0,0 +1,39 @@
+import { css } from "lit";
+
+export const styles = css`
+	.chat-timestamp-wrapper {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 4px;
+		margin-top: 148px;
+		margin-bottom: 32px;
+	}
+
+	.timestamp-left-line {
+		background: linear-gradient(
+			90deg,
+			rgba(178, 202, 231, 0.5) 0.68%,
+			rgba(181, 205, 247, 0.15) 97.3%
+		);
+		height: 2px;
+		width: 100px;
+	}
+
+	.timestamp {
+		font-weight: 500;
+		font-size: 12px;
+		line-height: 18px;
+		color: #4f585e;
+	}
+
+	.timestamp-right-line {
+		height: 2px;
+		width: 100px;
+		background: linear-gradient(
+			90deg,
+			rgba(178, 202, 231, 0.5) 0.68%,
+			rgba(181, 205, 247, 0.15) 97.3%
+		);
+	}
+`;
