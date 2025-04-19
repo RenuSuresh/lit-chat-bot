c499df39e6eba903bb92e5290f12cc90e49917c9
RenuPE101 <renu.ganvir@pharmeasy.in>
2025-04-19 17:14:34 +0530

    auto container height

--- src/components/ai-chat/index.ts
+++ src/components/ai-chat/index.ts
@@ -3,7 +3,7 @@ import { customElement, property, state } from "lit/decorators.js";
 import { commonStyles } from "./styles.css";
 import { DEFAULT_IMAGES } from "./constants";
 import { withChatContext } from "./context/with-chat-context";
-import type { ChatApiBody, ChatMessage, Theme } from "./theme.interface";
+import type { ChatApiBody, Theme } from "./theme.interface";
 
 // Services
 import { chatBotApi } from "../services/chat.service";
